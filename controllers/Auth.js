2a7ad4651c39f0aec6f89023b90457be12cb04c6
Alif Dhimas <alifdhimasctr@gmail.com>
2023-11-24 00:40:45 +0700

    nambahin hai di auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -5,6 +5,9 @@ import argon2 from "argon2";
 import jwt from "jsonwebtoken";
 import Operator from "../models/OperatorModel.js";
 
+
+//HAIIIII
+
 export const Login = async (req, res) => {
   console.log(req.body.email);
   const user = await User.findOne({
